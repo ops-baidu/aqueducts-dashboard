61eacb59b5bb3d8fd1b729f014aa4ee24a2c18ba
qudongfang <qudongfang@gmail.com>
2014-03-14 10:28:55 +0800

    switch kafka to kafka 0.8

--- jobs/kafka.rb
+++ jobs/kafka.rb
@@ -1,13 +1,25 @@
 @numOfPoints = 15
-@kafkaHost = "dbl-aqueducts-kafka01.dbl01.baidu.com"
+#@kafkaHost = "dbl-aqueducts-kafka01.dbl01.baidu.com"
+#@kafkaHosts = ["cq01-aqueducts-kafka00.cq01.baidu.com","cq01-aqueducts-kafka01.cq01.baidu.com", "cq01-aqueducts-kafka03.cq01.baidu.com", "cq01-aqueducts-kafka00.cq04.baidu.com","cq01-aqueducts-kafka05.cq01.baidu.com", "cq01-aqueducts-kafka06.cq01.baidu.com"]
+#@kafkaHost = "cq01-aqueducts-kafka00.cq01.baidu.com"
+@kafkaHosts = ['cq01-aqueducts-kafka00.cq01', 'cq01-aqueducts-kafka02.cq01', 'cq01-aqueducts-kafka03.cq01', 'cq01-aqueducts-kafka04.cq01', 'cq01-aqueducts-kafka05.cq01', 'cq01-aqueducts-kafka06.cq01', 'db-aqueducts-kafka08.db01', 'db-aqueducts-kafka07.db01', 'cq01-aqueducts-kafka01.cq01'] 
 @kafkaPort = "8888"
 @jmxcmdPath = "/home/work/bin/jmxcmd.jar"
 @jmxBean = "kafka:type=kafka.BrokerAllTopicStat"
-@period = 3
+@period = 5
 
-def getKafkaStatus(bean, key)
-  value = `java -jar #{@jmxcmdPath} - #{@kafkaHost}:#{@kafkaPort} #{bean} #{key} 2>&1`
-  return value.split(": ").last.to_i;
+@backup = 2 # kafka backup number
+
+def getKafkaStatus(bean, kafkaHosts)
+#  value = `java -jar #{@jmxcmdPath} - #{kafkaHosts}:#{@kafkaPort} '"kafka.server":name="#{bean}",type="BrokerTopicMetrics"' Count 2>&1`
+#  return value.split(":").last.to_i
+
+  total = 0
+  kafkaHosts.each do |host|
+    value = `java -jar #{@jmxcmdPath} - #{host}:#{@kafkaPort} '"kafka.server":name="#{bean}",type="BrokerTopicMetrics"' Count 2>&1`
+    total += value.split(": ").last.to_i;
+  end
+  return total
 end
 
 bytesIn = []
@@ -18,18 +30,18 @@ pointsIn = []
 pointsOut = []
 pointsMessagesIn = []
 
-bytesIn <<  getKafkaStatus(@jmxBean, "BytesIn")
-bytesOut << getKafkaStatus(@jmxBean, "BytesOut")
-messagesIn << getKafkaStatus(@jmxBean, "MessagesIn")
+bytesIn << getKafkaStatus("AllTopicsBytesInPerSec", @kafkaHosts)
+bytesOut << getKafkaStatus("AllTopicsBytesOutPerSec", @kafkaHosts)
+messagesIn << getKafkaStatus("AllTopicsMessagesInPerSec", @kafkaHosts)
 
 (1..@numOfPoints).each do |i|
-  bytesIn << getKafkaStatus(@jmxBean, "BytesIn")
-  bytesOut << getKafkaStatus(@jmxBean, "BytesOut")
-  messagesIn << getKafkaStatus(@jmxBean, "MessagesIn")
+  bytesIn << getKafkaStatus("AllTopicsBytesInPerSec", @kafkaHosts)
+  bytesOut << getKafkaStatus("AllTopicsBytesOutPerSec", @kafkaHosts)
+  messagesIn << getKafkaStatus("AllTopicsMessagesInPerSec", @kafkaHosts)
 
   pointsIn << { x: i, y: (bytesIn[i] - bytesIn[i - 1] ) / @period }
-  pointsOut << { x: i, y: (bytesOut[i] - bytesOut[i - 1]) / @period }
-  pointsMessagesIn << { x: i, y: (messagesIn[i] - messagesIn[i - 1]) / @period }
+  pointsOut << { x: i, y: (bytesOut[i] - bytesOut[i - 1]) / @period / @backup }
+  pointsMessagesIn << { x: i, y: (messagesIn[i] - messagesIn[i - 1]) / @period /@backup }
 end
 
 last_x = pointsIn.last[:x]
@@ -45,15 +57,20 @@ SCHEDULER.every "#{@period}s", allow_overlapping: false do
 
   last_x += 1
 
-  bytesIn << getKafkaStatus(@jmxBean, "BytesIn")
-  bytesOut << getKafkaStatus(@jmxBean, "BytesOut")
-  messagesIn << getKafkaStatus(@jmxBean, "MessagesIn")
+  bytesIn << getKafkaStatus("AllTopicsBytesInPerSec", @kafkaHosts)
+  bytesOut << getKafkaStatus("AllTopicsBytesOutPerSec", @kafkaHosts)
+  messagesIn << getKafkaStatus("AllTopicsMessagesInPerSec", @kafkaHosts)
 
   pointsIn << {x: last_x, y: (bytesIn[@numOfPoints] - bytesIn[@numOfPoints - 1]) / @period }
-  pointsOut << {x: last_x, y: (bytesOut[@numOfPoints] - bytesOut[@numOfPoints - 1]) / @period }
-  pointsMessagesIn << {x: last_x, y: (messagesIn[@numOfPoints] - messagesIn[@numOfPoints - 1]) / @period }
+  pointsOut << {x: last_x, y: (bytesOut[@numOfPoints] - bytesOut[@numOfPoints - 1]) / @period / @backup }
+  pointsMessagesIn << {x: last_x, y: (messagesIn[@numOfPoints] - messagesIn[@numOfPoints - 1]) / @period / @backup }
 
   send_event('bytesIn', points: pointsIn)
   send_event('bytesOut', points: pointsOut)
   send_event('messagesIn', points: pointsMessagesIn)
 end
+
+
+# java -jar ~/nfs/jmxcmd.jar - cq01-aqueducts-kafka00.cq01:8888 '"kafka.server":name="AllTopicsBytesInPerSec",type="BrokerTopicMetrics"' OneMinuteRate
+# java -jar ~/nfs/jmxcmd.jar - cq01-aqueducts-kafka00.cq01:8888 '"kafka.server":name="AllTopicsBytesOutPerSec",type="BrokerTopicMetrics"' OneMinuteRate
+# java -jar ~/nfs/jmxcmd.jar - cq01-aqueducts-kafka00.cq01:8888 '"kafka.server":name="AllTopicsMessagesInPerSec",type="BrokerTopicMetrics"' OneMinuteRate
---
+++ jobs/latency.rb
@@ -0,0 +1,25 @@
+require 'open-uri'
+require "json"
+
+def latency
+  url = "http://api.aqueducts.baidu.com/v1/events?product=im&service=router&item=page_view&calculation=count&from=-15s&to=now&detail=true"
+  result = JSON.parse(open(url).read)
+  last_point = result.first
+  insert_time = last_point["insert_time"].to_i / 1000
+  event_time = last_point["event_time"].to_i / 1000
+  latency = insert_time - event_time
+end
+
+points = []
+(1..10).each do |i|
+  points << { x: i, y: latency }
+end
+last_x = points.last[:x]
+
+SCHEDULER.every '20s' do
+  points.shift
+  last_x += 1
+  points << { x: last_x, y: latency }
+
+  send_event('latency', points: points)
+end
